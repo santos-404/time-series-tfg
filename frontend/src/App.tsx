fe78ec692177a26122bcde961827c3fc213f349e
Javier Santos <javsanmar5@alum.us.es>
2025-05-28 16:49:20 +0200

    Implement react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import "./index.css";
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/layout/navbar';
 import { Footer } from './components/layout/footer';
 import TimeSeriesDashboard from "./components/dashboard/TimeSeriesDashboard";
 
 export const App: React.FC = () => (
-  <div className="flex flex-col min-h-screen">
-    <Navbar />
-    <main className="flex-grow">
-      <TimeSeriesDashboard />
-    </main>
-    <Footer />
-  </div>
+  <Router>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<TimeSeriesDashboard />} />
+          <Route path="/dashboard" element={<TimeSeriesDashboard />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  </Router>
 );
