5e4ad5ef523bbc8650a804a6423669611e6d261f
Javier Santos <javsanmar5@alum.us.es>
2025-05-30 18:59:39 +0200

    Update useFetch hook to accept a refetch callback

--- frontend/src/hooks/useFetch.tsx
+++ frontend/src/hooks/useFetch.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 type Data<T> = T | null;
 type ErrorType = Error | null;
@@ -6,7 +6,8 @@ type ErrorType = Error | null;
 interface Params<T> {
   data: Data<T>;
   loading: boolean;
-  error: ErrorType
+  error: ErrorType;
+  refetch: () => Promise<void>;
 }
 
 // just added url and jwt. but if more params are
@@ -16,37 +17,38 @@ export const useFetch = <T,>(url: string, jwt?: string): Params<T> => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<ErrorType>(null);
 
+  const fetchData = useCallback(async (): Promise<void> => {
+    setLoading(true);
+    setError(null);
+    // The example of the auth here is a bearer token
+    // Obviously this may be used with any type of auth.
+    // (even with no auth :v)
+    try {
+      const response = await fetch(url, {
+        headers: {
+          'Authorization': `Bearer ${jwt}`,
+        },
+      });
+
+      // This error can be managed as wanted
+      if (!response.ok)
+        throw new Error(`Error on the response from ${url}`)
+
+      const responseJson: T = await response.json();
+      setData(responseJson);
+      setError(null);
+
+    } catch (err){
+      setError(err as Error)
+    } finally {
+      setLoading(false);
+    }
+  }, [url, jwt]);
 
   useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      // The example of the auth here is a bearer token
-      // Obviously this may be used with any type of auth.
-      // (even with no auth :v)
-      try {
-        const response = await fetch(url, {
-          headers: {
-            'Authorization': `Bearer ${jwt}`,
-          },
-        });
-        
-        // This error can be managed as wanted
-        if (!response.ok)
-          throw new Error(`Error on the response from ${url}`)
-
-        const responseJson: T = await response.json();
-        setData(responseJson);
-        setError(null);
-
-      } catch (err){
-        setError(err as Error)
-      } finally {
-        setLoading(false);
-      }
-    }
-
     fetchData();
-  }, [url, jwt])
+  }, [fetchData])
 
-  return { data, loading, error };
+  return { data, loading, error, refetch: fetchData};
 }
 
